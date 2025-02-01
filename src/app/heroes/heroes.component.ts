54f689ca1bf03fd7a2d7f90ef9a87da0cfa67fae
Yulian Borges Lobaina <yborges2005@gmail.com>
2025-02-01 19:12:23 +0100

    feat: actualizar HeroesComponent para mostrar el héroe actual y hacerlo standalone

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-heroes',
+  standalone: true,
   imports: [],
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.css'
 })
 export class HeroesComponent {
-
+  hero = 'Windstorm';
 }
