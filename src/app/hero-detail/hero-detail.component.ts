8a5717a0c43a4fe8ba8f25fbcb4714c3d68f7351
Yulian Borges Lobaina <yborges2005@gmail.com>
2025-02-03 19:54:57 +0100

    feat: añadir soporte para entrada de héroe en el componente HeroDetail

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Hero } from '../hero';
+import { NgIf, UpperCasePipe } from '@angular/common';
+import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 
 @Component({
   selector: 'app-hero-detail',
-  imports: [],
+  standalone: true,
+  imports: [UpperCasePipe, NgIf, FormsModule],
   templateUrl: './hero-detail.component.html',
   styleUrl: './hero-detail.component.css'
 })
 export class HeroDetailComponent {
-
+  @Input() hero?: Hero;
 }
