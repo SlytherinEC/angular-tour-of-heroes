f79cf08e5bd7547bf2183b31ef6a200a72654249
Yulian Borges Lobaina <yborges2005@gmail.com>
2025-02-03 19:54:29 +0100

    feat: actualizar imports en el componente Heroes para incluir HeroDetailComponent

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
-import { NgFor, NgIf, UpperCasePipe } from '@angular/common';
-import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
+import { NgFor } from '@angular/common';
 import { HEROES } from '../mock-heroes';
+import { HeroDetailComponent } from "../hero-detail/hero-detail.component";
 
 @Component({
   selector: 'app-heroes',
   standalone: true,
-  imports: [UpperCasePipe, FormsModule, NgFor, NgIf],
+  imports: [  NgFor, HeroDetailComponent],
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.css'
 })
