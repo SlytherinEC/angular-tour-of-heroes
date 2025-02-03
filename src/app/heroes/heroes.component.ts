8ca316ab63e8776ea1791e4c7209be19a069e317
Yulian Borges Lobaina <yborges2005@gmail.com>
2025-02-03 14:07:15 +0100

    feat: actualizar componente Heroes para seleccionar héroes y mostrar detalles

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,22 +1,24 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
-import { NgFor, UpperCasePipe } from '@angular/common';
+import { NgFor, NgIf, UpperCasePipe } from '@angular/common';
 import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
   standalone: true,
-  imports: [UpperCasePipe, FormsModule, NgFor],
+  imports: [UpperCasePipe, FormsModule, NgFor, NgIf],
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.css'
 })
 
 export class HeroesComponent {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+
+  selectedHero?: Hero 
+
+  onSelect(hero : Hero){
+    this.selectedHero = hero;
+  }
 
   heroes = HEROES;
 }
