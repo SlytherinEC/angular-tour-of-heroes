fcb91294bdaeb7353e803f5941205cfe4987e939
Yulian Borges Lobaina <yborges2005@gmail.com>
2025-02-03 13:39:10 +0100

    feat: añadir detalles del héroe y campo de entrada para editar el nombre

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
 import { UpperCasePipe } from '@angular/common';
+import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 
 @Component({
   selector: 'app-heroes',
   standalone: true,
-  imports: [UpperCasePipe],
+  imports: [UpperCasePipe, FormsModule],
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.css'
 })
+
 export class HeroesComponent {
   hero: Hero = {
     id: 1,
