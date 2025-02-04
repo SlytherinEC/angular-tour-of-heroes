93819457b8a8f290e8a0e1a6a254904c0c5d2e1f
Yulian Borges Lobaina <yborges2005@gmail.com>
2025-02-04 20:14:20 +0100

    feat: añadir navegación y enrutamiento al componente principal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeroesComponent } from "./heroes/heroes.component";
 import { MessagesComponent } from './messages/messages.component';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeroesComponent, MessagesComponent],
+  imports: [MessagesComponent, RouterOutlet, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
