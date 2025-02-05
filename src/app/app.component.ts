a9c1f884316f5a11e7626714ea5e58f2b7c0c7f2
Yulian Borges Lobaina <yborges2005@gmail.com>
2025-02-05 22:29:56 +0100

    feat: eliminar importación no utilizada de HeroesComponent en app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { HeroesComponent } from "./heroes/heroes.component";
 import { MessagesComponent } from './messages/messages.component';
 import { RouterLink, RouterOutlet } from '@angular/router';
 
